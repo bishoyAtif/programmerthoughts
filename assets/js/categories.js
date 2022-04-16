25ee0776b19a7511b542dd2bed97fb93b23f4bcc
Bishoy Atif <bishooy.atif@gmail.com>
2022-04-16 13:24:34 +0200

    Update

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,4 +1,4 @@
-const categories = { fiction: [{ url: `/posts/the-purpose-of-education/`, date: `12 Dec 1948`, title: `The Purpose of Education`},],jekyll: [{ url: `/posts/jekyll-markdown/`, date: `04 Nov 2021`, title: `Jekyll Markdown`},{ url: `/posts/the-purpose-of-education/`, date: `12 Dec 1948`, title: `The Purpose of Education`},], }
+const categories = { fiction: [{ url: `http://localhost:4000/posts/the-purpose-of-education/`, date: `12 Dec 1948`, title: `The Purpose of Education`},],jekyll: [{ url: `http://localhost:4000/posts/jekyll-markdown/`, date: `04 Nov 2021`, title: `Jekyll Markdown`},{ url: `http://localhost:4000/posts/the-purpose-of-education/`, date: `12 Dec 1948`, title: `The Purpose of Education`},], }
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
